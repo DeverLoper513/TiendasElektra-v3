4bde7cb3c8268a7e60d95de68eae73987810fb93
DeverLoper513 <151453624+DeverLoper513@users.noreply.github.com>
2024-11-08 10:51:11 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "949632";
-  let InstanceId_PromotedProd = "949633";
+  let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
